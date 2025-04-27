881c63317ae9d5aaffe7db66f7319e7c99aaff5a
Sahran <94926019+SahranREAL@users.noreply.github.com>
2025-04-27 15:19:28 +0200

    try patch files limite sizes

--- lumenone.js
+++ lumenone.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 
 // Middleware configuration
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "10mb" }));
 app.use(
   session({
     secret: config.session.secret, // Use of the config.json secret key
