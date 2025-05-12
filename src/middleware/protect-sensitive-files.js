de5800e2d756ac71f8c39c7de04a7136ddfd302b
legeriergeek <102820839+legeriergeek@users.noreply.github.com>
2025-05-12 17:36:07 +0200

    Update protect-sensitive-files.js

--- src/middleware/protect-sensitive-files.js
+++ src/middleware/protect-sensitive-files.js
@@ -4,9 +4,17 @@ const fs = require("fs");
 const protectedFiles = ["php.ini"];
 
 function protectSensitiveFiles(baseDir) {
+  const basePath = path.resolve(baseDir);
+
   return (req, res, next) => {
-    const normalizedPath = path.normalize(req.path);
-    const requestedPath = path.join(baseDir, normalizedPath);
+    const decodedPath = decodeURIComponent(req.path);
+    const normalizedPath = path.normalize(decodedPath);
+    const requestedPath = path.join(basePath, normalizedPath);
+
+    // Assure que le chemin est bien dans le dossier de base
+    if (!requestedPath.startsWith(basePath)) {
+      return res.status(403).send("Access denied.");
+    }
 
     const isProtected = protectedFiles.some((filename) =>
       requestedPath.endsWith(filename)
@@ -26,3 +34,4 @@ function protectSensitiveFiles(baseDir) {
 }
 
 module.exports = { protectSensitiveFiles };
+
