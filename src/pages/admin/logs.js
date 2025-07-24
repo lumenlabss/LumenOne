6a2f6138184e757a68f2ef5ccc01b491e6c7e4f0
Sahran <94926019+SahranREAL@users.noreply.github.com>
2025-07-24 15:35:04 +0200

    Add admin logs page and sidebar link
    Introduced a new admin logs page with placeholder files and added a corresponding link in the sidebar. Also updated the settings page to display an error message about required restarts after modifications.

---
+++ src/pages/admin/logs.js
@@ -0,0 +1,1 @@
+console.log("pages/admin/logs.js loaded"); // To confirm that the page has been loaded correctly
---
+++ src/utils/logger.js
@@ -0,0 +1,1 @@
+console.log("utils/logger.js loaded"); // To confirm that the page has been loaded correctly
