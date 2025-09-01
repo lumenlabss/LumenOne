a0df156526044251fd2a4de64ad46212bbfcee6a
Sahran <chationofficiel@gmail.com>
2025-09-01 17:53:18 +0200

    Fix typo in comment for view configuration
    Corrected a spelling error in the comment describing view and static file configuration for improved code readability.

--- lumenone.js
+++ lumenone.js
@@ -72,7 +72,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// Configurin       g views and static files
+// Configuring views and static files
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
